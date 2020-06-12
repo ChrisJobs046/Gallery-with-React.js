288ae571fd2624ff90776f1ecd9b891588d89adf
ChrisJobs046 <ChrisJobs046@gmail.com>
2020-06-11 22:31:29 -0400

    archivo nuevo

---
+++ react-gallery/src/components/GalleryImage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class GalleryImage extends React.Component {
+
+	render() {
+
+		return(
+
+			<>
+
+			
+
+			
+
+	  		</>
+
+		)
+
+	}
+
+}
+
+export default GalleryImage;
